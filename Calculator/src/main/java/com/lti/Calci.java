9137406cfe77e212f10239ac30297b8f372d2863
Aditya99-tech <adityanagrajan@gmail.com>
2020-12-21 15:09:20 +0530

    added prod

--- Calculator/src/main/java/com/lti/Calci.java
+++ Calculator/src/main/java/com/lti/Calci.java
@@ -7,6 +7,9 @@ public class Calci {
 	public static int subs(int num1, int num2) {
 		return num1-num2;
 	}
+	public static int prod(int num1, int num2) {
+		return num1*num2;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sum="+add(10,20));
